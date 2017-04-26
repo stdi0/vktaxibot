8860d83c184605d75b6b349358687535b013d130
Denis <denisyurievich94@gmail.com>
2017-04-26 17:59:29 +0300

    update

--- bot/views.py
+++ bot/views.py
@@ -5,11 +5,11 @@ import os
 
 # Create your views here.
 def confirmation(request):
-    if request.method == "POST":
+    #if request.method == "POST":
         #data = request.body.decode('utf-8')
         #received_json_data = json.loads(data)
-        path = '/home/vktaxibot/vktaxibot/json.txt'
-        f = open(path, 'w')
-        f.write('Hello')
+    path = '/home/vktaxibot/vktaxibot/json.txt'
+    f = open(path, 'w')
+    f.write('Hello, world!')
     return HttpResponse("Hello, world!")
 

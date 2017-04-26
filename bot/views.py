022cafa100f3c315b6c7d34ddb3c66c9f816e1e1
Denis <denisyurievich94@gmail.com>
2017-04-26 18:14:03 +0300

    update

--- bot/views.py
+++ bot/views.py
@@ -4,12 +4,18 @@ import json
 import os
 
 # Create your views here.
+
+confirmation_token = 'd144b920'
+
 def confirmation(request):
-    #if request.method == "POST":
+    if request.method == "POST":
+        if request.POST['type'] == 'confirmation':
+            return HttpResponse(confirmation_token)
         #data = request.body.decode('utf-8')
         #received_json_data = json.loads(data)
-    path = '/home/vktaxibot/vktaxibot/json.txt'
-    f = open(path, 'w')
-    f.write('Hello, world!')
-    return HttpResponse("Hello, world!")
+    #path = '/home/vktaxibot/vktaxibot/json.txt'
+    #f = open(path, 'w')
+    #.write('Hello, world!')
+    return HttpResponse("No")
+    
 

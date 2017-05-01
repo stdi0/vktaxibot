340a83457f5720ee55e2d501c500132d1fa7a6da
Denis <denisyurievich94@gmail.com>
2017-05-01 16:18:40 +0300

    update

--- bot/views.py
+++ bot/views.py
@@ -243,7 +243,6 @@ def bot(request):
                             request = urllib.request.Request('https://api.vk.com/method/messages.send?user_id=' + str(user_id) + '&message=' + quote(output_message) + '&access_token=' + token)
                             resp = urllib.request.urlopen(request)
                             send_mail('Новый заказ', 'Создан новый заказ! http://vktaxibot.pythonanywhere.com/active_orders', settings.EMAIL_HOST_USER, ['vktaxibot@gmail.com'])
-                            break
                             return HttpResponse('ok')
                 
                 output_message = 'Заказ номер ' + str(stage3[0].id) + ': Извините, адрес(а) не найден(ы). Проверьте правильность написания. Если Вы уверены, что не ошиблись, напишите слово \"Исключение\". Для отмены заказа, напишите слово \"отмена\"'

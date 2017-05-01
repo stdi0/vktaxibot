035a0d24be7cb578a0b1faeeff7a9ebc5787e6eb
Denis <denisyurievich94@gmail.com>
2017-05-01 15:13:35 +0300

    update

--- post.py
+++ post.py
@@ -1,35 +1,38 @@
 import json
 import requests
 
-API_KEY = '76150e77f594d464ca0f2f580c9b7250e50295ec'
-BASE_URL = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/%s'
+#API_KEY = '76150e77f594d464ca0f2f580c9b7250e50295ec'
+#BASE_URL = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/%s'
 
-def suggest(query, resource):
-    url = BASE_URL % resource
-    headers = { 
-        'Authorization': 'Token %s' % API_KEY,
-        'Content-Type': 'application/json',
-    }
+def suggest():
+    url = 'http://codestats.pythonanywhere.com/stdi0/api_call/'
+#    headers = { 
+#        'Authorization': 'Token %s' % API_KEY,
+#        'Content-Type': 'application/json',
+#    }
     data = {
-        'query': query
+        'count': 10,
+        'password': 'joo0shaij'
     }
-    r = requests.post(url, data=json.dumps(data), headers=headers)
-    return r.json()
+    r = requests.post(url, data=json.dumps(data))
+    return r
     
-city = 'Мурманск'    
-result = suggest(city, 'address')
+#city = 'Мурманск'    
+result = suggest()
 
-for i in result.get('suggestions'):
-    if str(i['data']['city']).lower() == city.lower():
-        print(i['data']['city'])
-        break
-else:
-    print('None')
+print(result)
 
-address = 'Миронова, 33/1'
-result = suggest(city + ' ' + address, 'address')
-for i in result.get('suggestions'):
-   if i['data']['street'] is not None:
+#for i in result.get('suggestions'):
+#    if str(i['data']['city']).lower() == city.lower():
+#        print(i['data']['city'])
+#        break
+#else:
+#    print('None')
+
+#address = 'Миронова, 33/1'
+#result = suggest(city + ' ' + address, 'address')
+#for i in result.get('suggestions'):
+#   if i['data']['street'] is not None:
 
 
 #print(result)

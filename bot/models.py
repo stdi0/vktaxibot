13bfde13b2eecf654eadc6e09e071c0fa16b22cd
Denis <denisyurievich94@gmail.com>
2017-05-08 20:48:47 +0300

    update

--- bot/models.py
+++ bot/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Order(models.Model):
     pos = models.IntegerField(default=0)
     user_id = models.CharField(max_length=30, default='')
-    message = models.TextField(default=None)
+    message = models.TextField(default=None, null=True)
     date = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(default=1)
     #1 - active
